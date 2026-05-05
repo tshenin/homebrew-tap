8a26d1c577915f142ad042ba35fb26dedec42dc4
Timofey Shenin <t.shenin@users.noreply.github.com>
2026-05-05 20:39:48 +0200

    Point cask URL to tap-hosted releases

--- Casks/prlauncher.rb
+++ Casks/prlauncher.rb
@@ -2,10 +2,10 @@ cask "prlauncher" do
   version "1.0.0"
   sha256 "fbb1795cdac82e2c702cced51172e1bea83e7e616c68c13b1305651818282da9"
 
-  url "https://github.com/tshenin/launcher/releases/download/v#{version}/PRLauncher-#{version}.dmg"
+  url "https://github.com/tshenin/homebrew-tap/releases/download/v#{version}/PRLauncher-#{version}.dmg"
   name "PRLauncher"
   desc "Menu bar app for browsing and launching GitHub pull requests"
-  homepage "https://github.com/tshenin/launcher"
+  homepage "https://github.com/tshenin/homebrew-tap"
 
   livecheck do
     url :url
